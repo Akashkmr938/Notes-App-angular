c9635f66d1cb9ec95823f44a4ce29b879fd09518
akumar1 <akumar1@empoweredbenefits.com>
2020-08-05 17:18:50 +0530

    server changes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 var express = require("express");
+const { PollingWatchKind } = require("typescript");
 
 var app = express();
 app.use(express.json());
-var distDir = __dirname + "/dist/";
+var distDir = __dirname + "/dist/notes-app";
 app.use(express.static(distDir));
+
+const PORT = process.env.PORT || "8080";
+
+app.listen(PORT);
